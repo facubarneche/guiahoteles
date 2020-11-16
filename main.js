48391aae85ead0864aa921713070ac576eb1dea9
Facundo Barneche <facu_barneche@hotmail.com.ar>
2020-11-16 20:37:28 -0300

    se le agrego un poco de estilo, con carruseles, fotos, etc

---
+++ main.js
@@ -0,0 +1,9 @@
+let searchInput = document.getElementById('searchInput');
+let searchBtn = document.getElementById('searchBtn');
+
+const search = () =>{
+    searchBtn.addEventListener('click',()=>{
+        console.log("hola")
+    });
+};
+search();
