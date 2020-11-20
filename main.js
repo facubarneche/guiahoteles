f1bd6590e7ba99f3b83c712440ab420c103d08f5
Facundo Barneche <facu_barneche@hotmail.com.ar>
2020-11-19 21:35:30 -0300

    Se termino el diseño responsive, algunos retoques en el diseño y se le agrego mediaquery para escritorio

--- main.js
+++
@@ -1,9 +0,0 @@
-let searchInput = document.getElementById('searchInput');
-let searchBtn = document.getElementById('searchBtn');
-
-const search = () =>{
-    searchBtn.addEventListener('click',()=>{
-        console.log("hola")
-    });
-};
-search();
